dfd580fac4dad208849850ee53c9895c76996819
reslin <uwe.ploss@gmail.com>
2024-05-04 21:58:17 +0200

    add tests fpr extract_* functions

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -7,6 +7,8 @@ from textnode import (
     text_type_italic,
     text_type_code,
     split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
 )
 
 
@@ -44,7 +46,43 @@ class TestTextNode(unittest.TestCase):
             ],
             nodes,
         )
+
+    def test_inline_double_code(self):
+        node = TextNode("This is a `code1` and `code2` node", text_type_text)
+        nodes = split_nodes_delimiter([node], "`", text_type_code)
+        self.assertListEqual(
+            [
+                TextNode("This is a ", text_type_text),
+                TextNode("code1", text_type_code),
+                TextNode(" and ", text_type_text),
+                TextNode("code2", text_type_code),
+                TextNode(" node", text_type_text),
+            ],
+            nodes,
+        )
         
+    def test_extract_image(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+        tuples = extract_markdown_images(text)
+        self.assertListEqual(
+            [
+            ("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            ("another", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png"),
+            ],
+            tuples,
+        )
+
+    def test_extract_link(self):
+        text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+        tuples = extract_markdown_links(text)
+        self.assertListEqual(
+            [
+                ("link", "https://www.example.com"),
+                ("another", "https://www.example.com/another"),
+            ],
+            tuples,
+        )
+    # [("link", "https://www.example.com"), ("another", "https://www.example.com/another")]
 
 
 if __name__ == "__main__":
