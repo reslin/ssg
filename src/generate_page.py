9cc099937ff052caa5a75361d80c24017db705cb
reslin <uwe.ploss@gmail.com>
2024-05-09 23:43:07 +0200

    add recursive generation

--- src/generate_page.py
+++ src/generate_page.py
@@ -29,3 +29,20 @@ def generate_page(from_path, template_path, dest_path):
     dest_file = os.path.join(dest_path, "index.html")
     with open(dest_file, "w") as f:
         f.write(new_content)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+        print(f"made dir {dest_dir_path}")
+    dir_path_contents = list(map(lambda f: os.path.join(dir_path_content, f),
+                                  os.listdir(dir_path_content)))
+    for content in dir_path_contents:
+        if os.path.isdir(content):
+            dst_path = os.path.join(dest_dir_path, os.path.split(content)[1])
+            generate_pages_recursive(content, template_path, dst_path)
+        else:
+            generate_page(content, template_path, dest_dir_path)
+            print(f"generated from {content}...")
+            #shutil.copy(p, dst)
+            #print(f"copied {p} -> {dst}")
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from copystatic import copy_recursive
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
 def main():
     copy_recursive("static", "public")
-    generate_page("content/index.md", "template.html", "public")
+    #generate_page("content/index.md", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "public")
     
 main()
