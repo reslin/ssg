930a22c197ff659bf87a9e0362170b88af31a34d
reslin <uwe.ploss@gmail.com>
2024-10-23 08:23:07 +0200

    add tests

---
+++ src/test_textnode.py
@@ -0,0 +1,50 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq_normal(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertEqual(node, node2)
+
+    def test_eq_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD, None)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertEqual(node, node2)
+
+    def test_eq_code(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        node2 = TextNode("This is a text node", TextType.CODE)
+        self.assertEqual(node, node2)
+
+    def test_eq_link(self):
+        node = TextNode("This is a text node", TextType.LINK)
+        node2 = TextNode("This is a text node", TextType.LINK)
+        self.assertEqual(node, node2)
+
+    def test_eq_image(self):
+        node = TextNode("This is a text node", TextType.IMAGE)
+        node2 = TextNode("This is a text node", TextType.IMAGE)
+        self.assertEqual(node, node2)
+
+    def test_neq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.IMAGE)
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT.value, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,14 +1,15 @@
 from enum import Enum
 
+
 class TextType(Enum):
-    NORMAL = "normal"
-    BOLD = "auto"
+    TEXT = "text"
+    BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
-class TextNode():
+class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
@@ -17,7 +18,7 @@ class TextNode():
     def __eq__(self, other):
         return (
             self.text == other.text
-            and self.text_type == other.text_typ
+            and self.text_type == other.text_type
             and self.url == other.url
         )
 
